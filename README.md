f88976b896bdb651b2ba72ad76557366c1eb9f68
youssef taoussi <yousseftaoussi2003@gmail.com>
2026-07-14 03:01:05 +0100

    add resource and i will replement everything with finite state machine and constraint decoding for json

--- README.md
+++ README.md
@@ -1,17 +1,30 @@
 ## Resources
+
 https://medium.com/@adimodi96/from-logits-to-tokens-9a36feab9cab
+
 ## # how LLM work:
+
 how llm work: https://martinfowler.com/articles/function-call-LLM.html
+
 ## constraint decoding:
+
 https://www.salmanq.com/blog/llm-constrained-sampling/#
 https://mbrenndoerfer.com/writing/constrained-decoding-structured-llm-output
 
 ### best format for constraint decoding
+
 https://mbrenndoerfer.com/writing/constrained-decoding-structured-llm-output#the-mathematical-formulation
+
 ### end
 
-
 ### Prefix tree:
+
 https://polaris000.medium.com/understanding-prefix-trees-13da74b3cafb
 
 fine tuning = https://comfyai.app/article/llm-hands-on-practice/qwen3-ift
+
+### constraint decoding json:
+
+https://pr1nt.dev/posts/constrained-json-decoding/
+
+### x grammar: https://xgrammar.mlc.ai/docs/structural_tag/tool_calling_and_reasoning.html
--- test.py
+++ test.py
@@ -1,18 +1,39 @@
-from typing import final
-
-from typing_extensions import override
-
-
-class A:
-    def __init__(self) -> None:
-        self._hello()
-
-    def _hello(self) -> None:
-        print("hello")
+from pydantic import BaseModel
+from schema_converter import SchemaConverter
+import json
+import sys
+import argparse
 
 
-class B(A):
+class B(BaseModel):
+    function_name: str
+    parameters: dict[str, str]
 
-    @override
-    def _hello(self) -> None:
-        print("hello")
+
+def main() -> None:
+
+    parser = argparse.ArgumentParser(
+        description="""
+            Generates a grammar (suitable for use in ./llama-cli) that produces JSON conforming to a
+            given JSON schema. Only a subset of JSON schema features are supported; more may be
+            added in the future.
+        """,
+    )
+
+    parser.add_argument(
+        "--raw-pattern",
+        type=str,
+        help="Treats string patterns as raw patterns w/o quotes (or quote escapes)",
+    )
+
+    parser.add_argument("schema", help='file containing JSON schema ("-" for stdin)')
+    args = parser.parse_args(sys.argv)
+    model_json = json.dumps(B.model_json_schema(), indent=2)
+    # args.model_json = model_json
+    # schemaConvert = schemaConverter()
+    print(args.prop_order)
+
+    print()
+
+
+main()
